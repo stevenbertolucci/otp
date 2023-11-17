ebad5032a88dac20444ada043bf85df5d4d0f2ea
stevenbertolucci <steven.bertolucci@yahoo.com>
2023-11-17 15:46:50 -0800

    Keygen code added

--- keygen.c
+++ keygen.c
@@ -13,4 +13,30 @@
 //   must be output to stderr.
 // -----------------------------------------------------------------------------------------------
 
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]) {
+
+  srand(time(0));
+  int number = atoi(argv[1]);
+  char key[number++];                           /* For null terminator */
+  char* alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+  if (argc != 2)
+  {
+    printf("Usage: %s length\n", argv[0]);
+    exit(EXIT_SUCCESS);
+  }
+
+  for (int i = 0; i < number; i++)
+  {
+    key[i] = alpha[rand() % 27];
+  }
+
+  key[number] = '\0';
+
+  printf("%s\n", key);
+}
